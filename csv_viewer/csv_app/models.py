396aa61acfb8b678cc5af618f6cd04569a977e73
Julia132 <uka132@mail.ru>
2020-02-24 00:32:13 +0400

    created

--- csv_viewer/csv_app/models.py
+++ csv_viewer/csv_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class CSVRecord(models.Model):
+
+    code = models.TextField()
+    name = models.TextField()
+    level_one = models.TextField()
+    level_two = models.TextField()
+    level_three = models.TextField()
+    price = models.TextField()
+    bid_price = models.TextField()
+    number = models.TextField()
+    property_fields = models.TextField()
+    joint_purchase = models.TextField()
+    unit = models.TextField()
+    picture = models.TextField()
+    display_onscreen = models.TextField()
+    description = models.TextField()
+
+
